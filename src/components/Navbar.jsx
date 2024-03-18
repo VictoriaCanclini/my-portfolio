dfda6e91bd45d08d9400dd9df81b051c833eb23c
Victoria Canclini <vico_canclini@hotmail.com>
2024-03-18 18:12:21 -0300

    new files

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="nav-container">
+      <div className="logo">
+        <Link href="/">Victoria Canclini</Link>
+      </div>
+      <a
+        href="https://drive.google.com/file/d/12DF7E6KQDnzHYsbcTeZAoZx0zkoK_isO/view?usp=drive_link"
+        className="cta-btn"
+        target="_blank"
+      >
+        cv
+      </a>
+    </div>
+  );
+};
+
+export default Navbar;
