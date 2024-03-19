8d3cbab100fd32796e3e96bae228aa9e4e0c740f
Victoria Canclini <vico_canclini@hotmail.com>
2024-03-19 12:59:55 -0300

    styles ok

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,20 @@
+import React from "react";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
 
-import Head from "next/head";
+// import Head from "next/head";
 
 const Home = () => {
   return (
     <>
-      <Head>
-        <title>Joel's Portfolio</title>
-        <meta name="description" content="Joel's Portfolio" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div>
-        <Hero />
-        <About />
-        <Skills />
-        <Projects />
-        <Contact />
-      </div>
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </>
   );
 };
