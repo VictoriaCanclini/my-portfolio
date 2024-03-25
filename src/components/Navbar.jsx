274d6f5ddcbf1018f9aba12e47bb16650a8b33ad
Victoria Canclini <vico_canclini@hotmail.com>
2024-03-25 13:55:09 -0300

    avanced navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,18 @@
+"use client";
+
 import { Burguer, Download } from "@/common/icons";
 import Link from "next/link";
 import "../styles/globals.css";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    console.log("hola");
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="nav-container">
       <div className="logo text-[#c9a4e8]">
@@ -10,10 +20,11 @@ const Navbar = () => {
           <span className="text-[#c9a4e8]">Portfolio VC</span>
         </Link>
       </div>
+
       <div className="horizontal-list">
         <ul>
           <li>
-            <a href="/about">Acerca de mi</a>
+            <Link href="skills">Acerca de mi </Link>
           </li>
           <Link href="skills">
             <li>Habilidades</li>
@@ -35,9 +46,35 @@ const Navbar = () => {
         </ul>
       </div>
 
-      {/* <button className="cta-btn">
+      {/* <button onClick={toggleMenu} className="cta-btn">
         <Burguer />
-      </button> */}
+      </button>
+      {isMenuOpen && (
+        <div className="vertical-list">
+          <ul>
+            <li>
+              <Link href="skills">Acerca de mi </Link>
+            </li>
+            <Link href="skills">
+              <li>Habilidades</li>
+            </Link>
+            <Link href="projects">
+              <li>Proyectos</li>
+            </Link>
+            <a href="mailto:vikicanclini@gmail.com">
+              <li>Contacto</li>
+            </a>
+            <a
+              href="https://drive.google.com/file/d/12DF7E6KQDnzHYsbcTeZAoZx0zkoK_isO/view?usp=drive_link"
+              target="_blank"
+            >
+              <li>
+                <Download color="#c9a4e8" />
+              </li>
+            </a>
+          </ul>
+        </div>
+      )} */}
     </div>
   );
 };
