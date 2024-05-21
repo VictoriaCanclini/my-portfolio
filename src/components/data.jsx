351d86697d079468f906f69b91864e7afc55c917
Victoria Canclini <vico_canclini@hotmail.com>
2024-05-21 10:49:29 -0300

    modified technologies

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -3,7 +3,7 @@ export const projectData = [
     id: 1,
     title: "STUDIO BY M",
     description:
-      "Aplicación mobile/web para la compra de cursos de UX/UI utilizando Mercado pago y PayPal.\nTecnologías: HTML - CSS - JavaScript - Node.js - Express - MongoDB - Mongoose - React - Next.js - Redux - Tailwind CSS - Mercado pago - PayPal - Figma - Trello.",
+      "Aplicación mobile/web para la compra de cursos de UX/UI utilizando Mercado pago y PayPal.\nTecnologías: HTML5 - CSS - JavaScript - Node.js - Express - MongoDB - Mongoose - JWT - React - Next.js - Redux - Tailwind CSS - Mercado pago - PayPal - Figma - Trello.",
     demoLink: "https://studiobym.com.ar",
     imageSrc: "/images/Maca.png",
     gitHubLink: "https://github.com/flormartinez92/Studio-M-Client",
@@ -12,7 +12,7 @@ export const projectData = [
     id: 2,
     title: "PORTFOLIO",
     description:
-      "Mi Portfolio personal mobile/web donde conocerás un poco más de mí, aquí vas a encontrar mis proyectos y trabajos realizados en este último tiempo. Tecnologías: HTML - CSS puro - Tailwind CSS - JavaScript - React - Next.js 14, en donde aplique el dark-mode con la librería de next-themes.",
+      "Mi Portfolio personal mobile/web donde conocerás un poco más de mí, aquí vas a encontrar mis proyectos y trabajos realizados en este último tiempo. Tecnologías: HTML5 - CSS puro - JavaScript - React - Next.js 14, en donde aplique el dark-mode con la librería de next-themes.",
     demoLink: "",
     imageSrc: "/images/compu.jpeg",
     gitHubLink: "https://github.com/VictoriaCanclini/my-portfolio",
@@ -21,7 +21,7 @@ export const projectData = [
     id: 3,
     title: "BOX DELIVERY",
     description:
-      "Aplicación mobile de una empresa de logística de última milla que tiene cientos de repartidores que al inicio del día reciben x cantidad de paquetes cada uno y que deberán ir repartiendo a lo largo de la jornada. Tecnologías: HTML - CSS - TypeScript - Node.js - Nest.js - MongoDB - Mongoose - React - Next.js - Redux - Tailwind - Docker - AWS - GitHub Actions - Figma - Trello.",
+      "Aplicación mobile de una empresa de logística de última milla que tiene cientos de repartidores que al inicio del día reciben x cantidad de paquetes cada uno y que deberán ir repartiendo a lo largo de la jornada. Tecnologías: HTML5 - CSS - TypeScript - Node.js - Nest.js - MongoDB - Mongoose - React - Next.js - Redux - Tailwind CSS - Docker - AWS - GitHub Actions - Figma - Trello.",
     demoLink:
       "https://drive.google.com/file/d/1-JXRbz35e-1Ag4EnpI9rh4QFRqS7ecQj/view?usp=drive_link",
     imageSrc: "/images/box.png",
@@ -31,7 +31,7 @@ export const projectData = [
     id: 4,
     title: "DEVBOOKS",
     description:
-      "Aplicación web para adquirir libros de programación y tecnología. \nTecnologías: HTML - CSS - JavaScript - Node.js - Express - PostgreSQL - Sequelize - Context - React - Material UI.",
+      "Aplicación web para adquirir libros de programación y tecnología. \nTecnologías: HTML5 - CSS - JavaScript - Node.js - Express - PostgreSQL - Sequelize - React context - React - Material UI.",
     demoLink:
       "https://drive.google.com/file/d/1FDTm-7SWD_wi25ZOmx9v69kt7FuxiKIe/view?usp=drive_link",
     imageSrc: "/images/devBookLogo.png",
@@ -41,7 +41,7 @@ export const projectData = [
     id: 5,
     title: "TMDB",
     description:
-      "Aplicación web que contiene películas y series utilizando una API externa, donde se puede agregar y eliminar en una lista de favoritos las películas que son de tu interés.\nTecnologías: HTML - CSS - JavaScript - Node.js - Express - PostgreSQL - Sequelize - Context - React - Bulma CSS. ",
+      "Aplicación web que contiene películas y series utilizando una API externa, donde se puede agregar y eliminar en una lista de favoritos las películas que son de tu interés.\nTecnologías: HTML5 - CSS - JavaScript - Node.js - Express - PostgreSQL - Sequelize - React context - React - Bulma CSS. ",
     demoLink:
       "https://drive.google.com/file/d/1K529CsTpntmZiSXOqADjzd-m3IMDNiAp/view?usp=drive_link",
     imageSrc: "/images/TMDB.png",
@@ -51,7 +51,7 @@ export const projectData = [
     id: 6,
     title: "Game of Life",
     description:
-      "El Juego de la vida es un autómata celular y un juego de simulación diseñado por el matemático británico John Horton Conway en 1970. Consiste en un tablero cuadriculado donde cada celda puede estar viva o muerta, y evoluciona según un conjunto de reglas simples basadas en el estado de las celdas vecinas. \nTecnologías: HTML - CSS - JavaScript.",
+      "El Juego de la vida es un autómata celular y un juego de simulación diseñado por el matemático británico John Horton Conway en 1970. Consiste en un tablero cuadriculado donde cada celda puede estar viva o muerta, y evoluciona según un conjunto de reglas simples basadas en el estado de las celdas vecinas. \nTecnologías: HTML5 - CSS - JavaScript.",
     demoLink: "https://sweet-sprite-506630.netlify.app",
     imageSrc: "/images/juego-de-la-vida.png",
     gitHubLink:
@@ -61,7 +61,7 @@ export const projectData = [
     id: 7,
     title: "Color Game",
     description:
-      "Adiviná cual es el color correcto a través de su código RGB (red-green-blue).\nTecnologías: HTML - CSS - JavaScript.",
+      "Adiviná cual es el color correcto a través de su código RGB (red-green-blue).\nTecnologías: HTML5 - CSS - JavaScript.",
     demoLink: "https://fascinating-baklava-ba8684.netlify.app",
     imageSrc: "/images/color-game.png",
     gitHubLink: "https://github.com/VictoriaCanclini/color-game",
@@ -70,7 +70,7 @@ export const projectData = [
     id: 8,
     title: "Weather Channel",
     description:
-      "Descubrí el clima actual en cualquier ciudad del mundo ingresando su nombre.\nTecnologías: HTML - CSS - JavaScript.",
+      "Descubrí el clima actual en cualquier ciudad del mundo ingresando su nombre.\nTecnologías: HTML5 - CSS - JavaScript.",
     demoLink: "https://poetic-daifuku-01a3a0.netlify.app",
     imageSrc: "/images/weather-channel.png",
     gitHubLink: "https://github.com/VictoriaCanclini/weather-channel",
@@ -79,7 +79,7 @@ export const projectData = [
     id: 9,
     title: "To do List",
     description:
-      "Aplicación para agregar, editar y eliminar una lista de tareas.\nTecnologías: HTML - CSS - JavaScript.",
+      "Aplicación para agregar, editar y eliminar una lista de tareas.\nTecnologías: HTML5 - CSS - JavaScript.",
     demoLink: "https://loquacious-queijadas-56b057.netlify.app",
     imageSrc: "/images/to-do-list.png",
     gitHubLink: "https://github.com/VictoriaCanclini/to-do-list",
